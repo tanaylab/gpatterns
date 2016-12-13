2d212996398ae596d196325a8eef4ac7cece7f9d
Aviezer Lifshitz <18175550+aviezerl@users.noreply.github.com>
2016-12-13 13:51:14 +0200

    changed qqu to qqv

--- R/defs.R
+++ R/defs.R
@@ -8,7 +8,9 @@
 
 #' @export
 qq <- GetoptLong::qq
-qqu <- function(..., envir=parent.frame()) qq(..., collapse=FALSE, envir=envir)
+
+#' @export
+qqv <- function(text) {GetoptLong::qq(text, envir=parent.frame(), collapse=FALSE)}
 
 #' @export
 fwrite <- function(...) data.table::fwrite(...)
@@ -45,32 +47,32 @@ gpatterns.set_parallel <- function(thread_num) {
 
 
 # Tracks and table names
-.gpatterns.downsampled_track_name <- function(track, dsn) {qqu('@{track}.ds@{dsn}') }
-.gpatterns.cov_track_name <- function(track) { qqu('@{track}.cov') }
-.gpatterns.avg_track_name <- function(track) { qqu('@{track}.avg') }
+.gpatterns.downsampled_track_name <- function(track, dsn) {qqv('@{track}.ds@{dsn}') }
+.gpatterns.cov_track_name <- function(track) { qqv('@{track}.cov') }
+.gpatterns.avg_track_name <- function(track) { qqv('@{track}.avg') }
 .gpatterns.pat_cov_track_name <- function(track, pat_len) { paste0(track, '.pat_cov', pat_len) }
-.gpatterns.fid_track_name     <- function(track) { qqu('@{track}.fid')}
-.gpatterns.ncpg_track_name    <- function(track) { qqu('@{track}.ncpg')}
-.gpatterns.n_track_name       <- function(track) { qqu('@{track}.n')}
-.gpatterns.n0_track_name      <- function(track) { qqu('@{track}.n0')}
-.gpatterns.n1_track_name      <- function(track) { qqu('@{track}.n1')}
-.gpatterns.nx_track_name      <- function(track) { qqu('@{track}.nx')}
-.gpatterns.nc_track_name      <- function(track) { qqu('@{track}.nc')}
-.gpatterns.pat_meth_track_name    <- function(track) { qqu('@{track}.pat_meth')}
-.gpatterns.epipolymorphism_track_name <- function(track) { qqu('@{track}.epipoly')}
+.gpatterns.fid_track_name     <- function(track) { qqv('@{track}.fid')}
+.gpatterns.ncpg_track_name    <- function(track) { qqv('@{track}.ncpg')}
+.gpatterns.n_track_name       <- function(track) { qqv('@{track}.n')}
+.gpatterns.n0_track_name      <- function(track) { qqv('@{track}.n0')}
+.gpatterns.n1_track_name      <- function(track) { qqv('@{track}.n1')}
+.gpatterns.nx_track_name      <- function(track) { qqv('@{track}.nx')}
+.gpatterns.nc_track_name      <- function(track) { qqv('@{track}.nc')}
+.gpatterns.pat_meth_track_name    <- function(track) { qqv('@{track}.pat_meth')}
+.gpatterns.epipolymorphism_track_name <- function(track) { qqv('@{track}.epipoly')}
 
-.gpatterns.mix_qval_track_name <- function(track) { qqu('@{track}.mix_qval')}
-.gpatterns.mix_meth_track_name <- function(track) { qqu('@{track}.mix_meth')}
-.gpatterns.mix_unmeth_track_name <- function(track) { qqu('@{track}.mix_unmeth')}
-.gpatterns.gain_meth_track_name <- function(track) { qqu('@{track}.gain_meth')}
-.gpatterns.gain_unmeth_track_name <- function(track) { qqu('@{track}.gain_unmeth')}
-.gpatterns.loss_meth_track_name <- function(track) { qqu('@{track}.loss_meth')}
-.gpatterns.loss_unmeth_track_name <- function(track) { qqu('@{track}.loss_unmeth')}
-.gpatterns.center_meth_track_name <- function(track) { qqu('@{track}.center_meth_ones')}
-.gpatterns.center_unmeth_track_name <- function(track) { qqu('@{track}.center_unmeth_ones')}
-.gpatterns.center_uni_track_name <- function(track) { qqu('@{track}.center_uni_ones')}
-.gpatterns.gain_uni_track_name <- function(track) { qqu('@{track}.gain_uni')}
-.gpatterns.loss_uni_track_name <- function(track) { qqu('@{track}.loss_uni')}
+.gpatterns.mix_qval_track_name <- function(track) { qqv('@{track}.mix_qval')}
+.gpatterns.mix_meth_track_name <- function(track) { qqv('@{track}.mix_meth')}
+.gpatterns.mix_unmeth_track_name <- function(track) { qqv('@{track}.mix_unmeth')}
+.gpatterns.gain_meth_track_name <- function(track) { qqv('@{track}.gain_meth')}
+.gpatterns.gain_unmeth_track_name <- function(track) { qqv('@{track}.gain_unmeth')}
+.gpatterns.loss_meth_track_name <- function(track) { qqv('@{track}.loss_meth')}
+.gpatterns.loss_unmeth_track_name <- function(track) { qqv('@{track}.loss_unmeth')}
+.gpatterns.center_meth_track_name <- function(track) { qqv('@{track}.center_meth_ones')}
+.gpatterns.center_unmeth_track_name <- function(track) { qqv('@{track}.center_unmeth_ones')}
+.gpatterns.center_uni_track_name <- function(track) { qqv('@{track}.center_uni_ones')}
+.gpatterns.gain_uni_track_name <- function(track) { qqv('@{track}.gain_uni')}
+.gpatterns.loss_uni_track_name <- function(track) { qqv('@{track}.loss_uni')}
 
 .gpatterns.patterns_tab_name  <- function(track) { 'patterns'}
 .gpatterns.patterns_file_name <- function(track) { file.path(.gpatterns.base_dir(track), 'patterns.RData')}
