c9ab98b2fa8e734b131c458441489ed0c624b20b
Aviezer Lifshitz <aviezer.lifshitz@weizmann.ac.il>
2018-05-21 12:59:01 +0300

    pipeline dependencies fixes

--- R/gpatterns-package.r
+++ R/gpatterns-package.r
@@ -6,11 +6,16 @@
 #' @import tidyr
 #' @import dplyr
 #' @import purrr
+#' @import stringr
 #' @import ggplot2
 #' @importFrom crayon red
 #' @importFrom crayon blue
 #' @importFrom data.table fwrite
 #' @importFrom tibble as.tibble
+#' @importFrom tibble is.tibble
+#' @importFrom tibble has_name
+#' @importFrom readr write_tsv
+#' @importFrom readr write_csv
 #' @importFrom GetoptLong qq
 #' @importFrom glue glue
 #' @importFrom tibble as.tibble
